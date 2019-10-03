80eecbb2bca1a28504f328f95400c662769c1627
Jongyeon Yoon <jyoon901@wch136-15.cs.ucr.edu>
2019-10-03 15:30:44 -0700

    Completed part 2

--- turnin/[cslogin]_lab2_part2.c
+++ turnin/[cslogin]_lab2_part2.c
@@ -16,7 +16,7 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure Port A's 8 pins as inputs
 	DDRC = 0xFF; PORTC = 0X00; // Configure port C's 8 pins as outputs
 	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-	unsigned char cntavail;
+	unsigned char cntavail = 0x00;
 	while(1) {
 		// 1) Read input (PINA's rightmost 4 bits)
 		tmpA = PINA & 0x0F;
@@ -26,30 +26,32 @@ int main(void) {
 		switch(tmpA) {
 			//there is no car -> 4 spaces available (0000)
 			case 0x00:
-			cntavail = 0x04;
+			cntavail = (cntavail & 0XF0) | 0x04; //cccc0100
 			break;
 
 			//there is a car -> 3 spaces available
 			//0001 0010 0100 1000
 			case 0x01: case 0x02: case 0x04: case 0x08:
-			cntavail = 0x03;
+			cntavail = (cntavail & 0XF0) | 0x03; //cccc0011
 			break;
 			
 			//there are 2 cars -> 2 spaces available
 			//0011 0101 1001 0110 1010 1100
 			case 0x03: case 0x05: case 0x09: case 0x06: case 0x0A: case 0x0C:
-			cntavail = 0x02;
+			cntavail = (cntavail & 0XF0) | 0x02; //cccc0010
 			break;
 			
 			//there are 3 cars -> 1 space available
 			//0111 1011 1101 1110
 			case 0x07: case 0x0B: case 0x0D: case 0x0E:
-			cntavail = 0x01;
+			cntavail = (cntavail & 0XF0) | 0x01; //cccc0001
 			break;
 			
 			//there are 4 cars -> no space available (1111)
 			case 0x0F:
-			cntavail = 0x00;
+			cntavail = (cntavail & 0X70) | 0x00; //cccc0000
+			//set PC7 to 1 if the lot is full
+			cntavail  = cntavail | 0x80; //1ccc0000			
 			break;
 		}
 		//copy cntavail to PORTC
