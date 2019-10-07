13d567a64cd95a8fcecd929f7c5895ffb32b1dee
Jongyeon Yoon <jyoon901@wch136-35.cs.ucr.edu>
2019-10-07 12:03:26 -0700

    Completed part 5

---
+++ turnin/[jyoon901]_lab3_part5.c
@@ -0,0 +1,47 @@
+/*	Author: Jongyeon Yoon
+ *      Partner(s) Name: Weifeng
+ *	Lab Section: 021
+ *	Assignment: Lab #03  Exercise #4
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+	DDRD = 0x00; PORTD = 0xFF; // Configure Port D's 8 pins as inputs
+	DDRB = 0xFE; PORTB = 0X01; // Configure port B's left 7 pins as outputs and rightmost 1 pin as input
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpD = 0x00; // Temporary variable to hold the value of D
+	unsigned short passengerWeight = 0;
+
+	while(1) {
+		// 1) Read the inputs
+		tmpD = PIND;
+		tmpB = PINB & 0x01;
+
+		// 2) Compute
+
+		//compute weight and store
+		//inputs are PD7..PD0PB0
+		passengerWeight = tmpD * 2 + tmpB;
+
+		//compute outputs of port B
+		if(passengerWeight >= 70) { //if the weight is equal to or above 70 pounds
+			tmpB = tmpB | 0x02; //set PB1 to 1
+		} else if(passengerWeight < 70 && passengerWeight > 5) {
+			tmpB = tmpB | 0x04; //set PB2 to 1
+		} else {
+			//neither B1 nor B2 should be set if the weight is 5 or less
+			//do nothing
+		}
+
+		// 3) Write output
+		PORTB = tmpB;
+	}
+	return 0;
+}
--- turnin/[jyoon901]_lab3_part5_tests.gdb
+++ turnin/[jyoon901]_lab3_part5_tests.gdb
@@ -27,25 +27,52 @@ echo ======================================================\n
 echo Running all tests..."\n\n
 
 # Add tests below
-test "PINA: 0xDE => PINB: 0x0D, PORTC: 0xE0"
-setPINA 0xDE
+#weight is 3
+test "PIND: 0x00, PINB: 0x01 => PORTB: 0x01"
+setPIND 0x00
+setPINB 0x01
 continue 5
-expectPORTB 0x0D
-expectPORTC 0xE0
+expectPORTB 0x01
 checkResult
 
-test "PINA: 0x3A => PINB: 0x03, PORTC: 0xA0"
-setPINA 0x3A
+#weight is 2
+test "PIND: 0x01, PINB: 0x00 => PORTB: 0x00"
+setPIND 0x01
+setPINB 0x00
+continue 5
+expectPORTB 0x00
+checkResult
+
+#weight is 30
+test "PIND: 0x0F, PINB: 0x00 => PORTB: 0x04"
+setPIND 0x0F
+setPINB 0x00
+continue 5
+expectPORTB 0x04
+checkResult
+
+#weight is 51
+test "PIND: 0x19, PINB: 0x01 => PORTB: 0x05"
+setPIND 0x19
+setPINB 0x01
+continue 5
+expectPORTB 0x05
+checkResult
+
+#weight is 500
+test "PIND:0xFC, PINB: 0x00 => PORTB: 0x02"
+setPIND 0xFC
+setPINB 0x00
+continue 5
+expectPORTB 0x02
+checkResult
+
+#weight is 75
+test "PIND: 0x25, PINB: 0x01 => PORTB: 0x03"
+setPIND 0x25
+setPINB 0x01
 continue 5
 expectPORTB 0x03
-expectPORTC 0xA0
-checkResult
-
-test "PINA: 0xFD => PINB: 0x0F, PORTC: 0xD0"
-setPINA 0xFD
-continue 5
-expectPORTB 0x0F
-expectPORTC 0xD0
 checkResult
 
 # Report on how many tests passed/tests ran
